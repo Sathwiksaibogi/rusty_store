9bc27bf99a9bb1ebe657b7dce7416943c9197290
Sathwiksaibogi <sathwiksaibogi05@gmail.com>
2026-05-25 00:41:27 +0530

    fourth commit

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,18 @@ fn main() {
     let mut my_store=Store::new();
     println!("rusty_store is opened!!!");
 
-    match my_store.add_product(101,String::from("laptop"),String::from("high speed"), 70000.00, 100){
+    // add products
+    match my_store.add_product(101,String::from("laptop"),String::from("high speed"), 70000.00,90000.00, 100){
         Ok(_)=>println!("product added successfully"),
         Err(e)=>println!("{}",e),
     }
 
-    match my_store.add_product(101,String::from("laptop2"),String::from("light weight"), 50000.00, 50){
+    match my_store.add_product(101,String::from("laptop2"),String::from("light weight"), 50000.00,70000.00, 50){
         Ok(_)=>println!("product overwrote"),
         Err(e)=>println!("{}",e),
     }
 
+    // edit products
     match my_store.edit_product(101, 75000.00, 150){
         Ok(_)=>println!("product is updated"),
         Err(e)=>println!("{}",e),
@@ -24,6 +26,7 @@ fn main() {
         Err(e)=>println!("{}",e),
     }
 
+    // delete products
     match my_store.del_product(101){
         Ok(_)=>println!("product got deleted"),
         Err(e)=>println!("{}",e),
@@ -32,4 +35,25 @@ fn main() {
         Ok(_)=>println!("product got deleted"),
         Err(e)=>println!("{}",e),
     }
+
+    // sell products
+    match my_store.record_sale(1,101,2){
+        Ok(_)=>println!("product sold"),
+        Err(e)=>println!("{}",e),
+    }
+    match my_store.record_sale(1,101,200){
+        Ok(_)=>println!("product sold"),
+        Err(e)=>println!("{}",e),
+    }
+
+    // purchase products
+    match my_store.record_purchase(1, 101, 100, 80000.00){
+        Ok(_)=>println!("product purchased"),
+        Err(e)=>println!("{}",e),
+    }
+    match my_store.record_purchase(1, 1010, 100, 80000.00){
+        Ok(_)=>print!("product purchased"),
+        Err(e)=>println!("{}",e),
+    }
+
 }
--- src/models.rs
+++ src/models.rs
@@ -3,21 +3,22 @@ pub struct Product{
     pub id:u32,
     pub name:String,
     pub description:String,
-    pub price:f64,
+    pub cost_price:f64,
+    pub sell_price:f64,
     pub quantity:u32,
 }
 pub struct Sale{
     pub sale_id:u32,
     pub product_id:u32,
     pub quantity:u32,
-    pub price:f64,
+    pub sell_price:f64,
 
 }
 pub struct Purchase{
     pub purchase_id:u32,
     pub product_id:u32,
     pub quantity:u32,
-    pub price:f64,
+    pub cost_price:f64,
 }
 pub struct Store{
     pub products:HashMap<u32,Product>,
@@ -35,12 +36,13 @@ impl Store{
          }
     }
 
-    pub fn add_product(&mut self,id:u32,name:String,description:String,price:f64,quantity:u32)->Result<(),String>{
+    pub fn add_product(&mut self,id:u32,name:String,description:String,cost_price:f64,sell_price:f64,quantity:u32)->Result<(),String>{
         let product=Product{
             id,
             name,
             description,
-            price,
+            cost_price,
+            sell_price,
             quantity,
         };
         if self.products.contains_key(&id){
@@ -54,7 +56,7 @@ impl Store{
     pub fn edit_product(&mut self,id:u32,new_price:f64,new_quantity:u32)->Result<(),String>{
         match self.products.get_mut(&id){
             Some(product)=>{
-                product.price=new_price;
+                product.sell_price=new_price;
                 product.quantity=new_quantity;
                 Ok(())
             }
@@ -73,6 +75,55 @@ impl Store{
             }
             None=>{
                 Err(String::from("product does not exist"))
+            }
+        }
+    }
+
+    pub fn record_sale(&mut self,sale_id:u32,product_id:u32,quantity:u32)->Result<(),String>{
+        match self.products.get_mut(&product_id){
+            Some(product)=>{
+                if product.quantity >= quantity {
+                    product.quantity-=quantity;
+                    let total_revenue=(quantity as f64) *product.sell_price;
+                    let total_costprice=(quantity as f64)*product.cost_price;
+                    let profit=total_revenue-total_costprice;
+                    let sale=Sale{
+                        sale_id,
+                        product_id,
+                        quantity,
+                        sell_price:product.sell_price,
+        
+                    };
+                    self.sales.push(sale);
+                    println!("the profit for the sale {} is {}",sale_id,profit);
+                    Ok(())
+                }else{
+                    Err(String::from("insufficient stock"))
+                }
+            }
+            None=>{
+                Err(String::from("product not found"))
+            }
+        }
+    }
+
+    pub fn record_purchase(&mut self,purchase_id:u32,product_id:u32,quantity:u32,new_cost_price:f64)->Result<(),String>{
+        match self.products.get_mut(&product_id){
+            Some(product)=>{
+                product.quantity+=quantity;
+                product.cost_price=new_cost_price;
+                let purchase=Purchase{
+                    purchase_id,
+                    product_id,
+                    quantity,
+                    cost_price:new_cost_price,
+                };
+                self.purchases.push(purchase);
+                Ok(())
+
+            }
+            None=>{
+                Err(String::from("product must be added in store first"))
             }
         }
     }
